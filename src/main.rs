a33fe64875f517475badfde2aac663ab5767797b
heningwei@126.com <heningwei@126.com>
2022-07-09 15:11:53 +0800

    完成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let arr: [u32;4] = [1,2,3,4];
+    println!("sum is {:?}", sum(&arr));
+
+    let arr: [u32;4] = [4000000000,4000000000,4000000000,4000000000];
+    println!("sum is {:?}", sum(&arr));
+}
+
+fn sum(arr: &[u32]) -> Option<u32>{
+    let mut result: u32 = 0;
+    let mut tag: bool = false;
+    for a in arr {
+        (result, tag) = result.overflowing_add(*a);
+        if tag == true {
+            return None;
+        }
+    }
+    Some(result)
 }
